659d4ee4d5afc3f3fe9b8e24e1082f32c8b4c108
Gabriel Schenato <gabrielschenato152@hotmail.com>
2023-12-20 14:36:39 -0300

    Criando UseCase CreateClient

---
+++ internal/usecase/create_client/create_client.go
@@ -0,0 +1,49 @@
+package createclient
+
+import (
+	"time"
+
+	"github.com.br/GabrielSchenato/wallet-core/internal/entity"
+	"github.com.br/GabrielSchenato/wallet-core/internal/gateway"
+)
+
+type CreateClientInputDTO struct {
+	Name  string
+	Email string
+}
+
+type CreateClientIOutputtDTO struct {
+	ID        string
+	Name      string
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type CreateClientUseCase struct {
+	ClientGateway gateway.ClientGateway
+}
+
+func NewCreateClientUseCase(clientGateway gateway.ClientGateway) *CreateClientUseCase {
+	return &CreateClientUseCase{
+		ClientGateway: clientGateway,
+	}
+}
+
+func (uc *CreateClientUseCase) Execute(input CreateClientInputDTO) (*CreateClientIOutputtDTO, error) {
+	client, err := entity.NewClient(input.Name, input.Email)
+	if err != nil {
+		return nil, err
+	}
+	err = uc.ClientGateway.Save(client)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateClientIOutputtDTO{
+		ID:        client.ID,
+		Name:      client.Name,
+		Email:     client.Email,
+		CreatedAt: client.CreatedAt,
+		UpdatedAt: client.UpdatedAt,
+	}, nil
+}
